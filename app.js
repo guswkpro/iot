4d3c3f3dd30c49640301bcd8c3a57b93068ea589
guswkpro <guswk_pro@naver.com>
2018-11-28 22:16:44 +0900

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 var port = process.env.PORT || 3000; //*
 
 var server = app.listen(port, function () {
-        console.log("Express server has started on port 3000")
+	console.log("Express server has started on port 3000")
 });
 
 var bodyParser = require('body-parser');
@@ -19,72 +19,59 @@ app.use(bodyParser.json());
 var mysql = require('mysql');
 
 var client = mysql.createConnection({
-    host: 'stweb.ccmxaq6oosug.ap-northeast-2.rds.amazonaws.com'
-    , port: 3306
-    , user: 'stweb'
-    , password: 'stwebstweb'
-    , database: 'okgoogle'
+	host: 'stweb.ccmxaq6oosug.ap-northeast-2.rds.amazonaws.com'
+	, port: 3306
+	, user: 'stweb'
+	, password: 'stwebstweb'
+	, database: 'okgoogle'
 });
 
 /********************
         GET
 ********************/
-app.get('/', function(req, res){
-	client.query('UPDATE okgoogle.okgoogle_datas set data_number = ? where data_idx = ?', [Number(req.param("type")), Number(1)], function(error, result){
-		if(error){
+app.get('/', function (req, res) {
+	client.query('UPDATE okgoogle.okgoogle_datas set data_number = ? where data_idx = ?', [Number(req.param("type")), Number(1)], function (error, result) {
+		if (error) {
 			console.log(error);
 			res.json({
-				RESULT : "0",
-				ERROR : error
+				RESULT: "0",
+				ERROR: error
 			});
 		} else {
 			res.json({
-				RESULT : "1"
+				RESULT: "1"
 			});
 		}
 	});
 });
-app.post('/', function(req, res){
-	client.query('UPDATE okgoogle.okgoogle_datas set data_number = ? where data_idx = ?', [Number(req.param("type")), Number(1)], function(error, result){
-		if(error){
+app.post('/', function (req, res) {
+	if (req.body.value == "콜라") {
+		res.json({
+			speech: "시원한 콜라 한캔 받으세요.",
+			displayText: "시원한 콜라 한캔 받으세요.",
+			RESULT: "1"
+		});
+	} else if (req.body.value == "사이다") {
+		res.json({
+			speech: "시원한 사이다 한캔 받으세요.",
+			displayText: "시원한 사이다 한캔 받으세요.",
+			RESULT: "2"
+		})
+	}
+});
+
+app.get('/getdata', function (req, res) {
+	client.query('SELECT * from okgoogle.okgoogle_datas', [], function (error, result) {
+		if (error) {
 			console.log(error);
 			res.json({
-				RESULT : "0",
-				ERROR : error
+				RESULT: "0",
+				ERROR: error
 			});
 		} else {
 			res.json({
-				RESULT : "1"
+				RESULT: result
 			});
 		}
-	});
-});
-
-app.get('/test', function(req, res){
-	client.query('SELECT * from okgoogle.okgoogle_datas', [], function(error, result){
-		if(error){
-			console.log(error);
-			res.json({
-				RESULT : "0",
-				ERROR : error
-			});
-		} else {
-			res.json({
-				RESULT : result
-			});
-		}
-	});
-});
-
-app.get('/test2', function(req, res){
-	    res.writeHead(200, {'Content-Type' : 'text/html'});
-    res.write('<h3>Welcome</h3>');
-    res.write('<a href="/login">Please login</a>');
-    res.end();
-});
-
-app.get('/test3', function(req, res){
-	res.json({
-		RESULT : "1"
 	});
 });
